fc440fe512d341358979a34c45839bffa2b3e464
marcelsguima <marcelsguima@gmail.com>
2023-03-02 04:06:12 -0300

    Fix sales route to evaluate

--- src/routes/sales.routes.js
+++ src/routes/sales.routes.js
@@ -3,7 +3,7 @@ const salesController = require('../controllers/sales.controller');
 
 const salesRouter = express.Router();
 
-salesRouter.post('/', salesController.registerSale);
+// salesRouter.post('/', salesController.registerSale);
 salesRouter.get('/', salesController.findAllSales);
 salesRouter.get('/:id', salesController.saleByID);
 
